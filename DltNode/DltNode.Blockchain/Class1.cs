5a5df67e0993491adabf1b60f8f0820dfded1796
dr.lis <dr.lis@outlook.com>
2022-10-27 02:25:00 +0300

    Inited

---
+++ DltNode/DltNode.Blockchain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DltNode.Blockchain
+{
+	public class Class1
+	{
+	}
+}
---
+++ DltNode/DltNode.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DltNode.Main
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
