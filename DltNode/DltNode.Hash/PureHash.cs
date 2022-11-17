47fc77c3526b150edc11751215436062fa4e2376
dr.lis <dr.lis@outlook.com>
2022-11-17 05:00:19 +0300

    Template for laba1

--- DltNode/DltNode.Hash/PureHash.cs
+++ DltNode/DltNode.Hash/PureHash.cs
@@ -4,12 +4,15 @@ namespace DltNode.Hash
 {
 	public class PureHash
 	{
-		public uint[] h = { 0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19};
+		//public uint[] h = { 0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19};
 
 		public byte[] computeHash(byte[] input)
 		{
+			//input.Length 
+
 			byte[] output = new byte[32];
 
+			// implement your function here
 
 
 			return output;
