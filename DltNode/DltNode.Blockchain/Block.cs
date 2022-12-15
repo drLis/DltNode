9a76f55928cc622d17d84d9420ccd9b67b4a313c
dr.lis <dr.lis@outlook.com>
2022-12-15 04:58:29 +0300

    Completed Block class

---
+++ DltNode/DltNode.Blockchain/Block.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DltNode.Hash;
+
+namespace DltNode.Blockchain
+{
+	public class Block
+	{
+		public readonly Byte[] blockHash;
+
+		private readonly Byte[] zero;
+
+		public readonly Byte[] previousBlockHash;
+
+		public readonly List<Transaction> transactions;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="transactions"></param>
+		/// <param name="previousBlock">Put null in to first block constructor</param>
+		public Block(List<Transaction> transactions, Block previousBlock)
+		{
+			zero = new byte[32];
+			this.previousBlockHash = previousBlock?.blockHash ?? zero;
+			this.transactions = transactions;
+
+			List<Byte[]> hashes = new List<Byte[]>();
+			hashes.Add(previousBlockHash);
+			foreach (var tx in transactions)
+			{
+				hashes.Add(tx.GetHash());
+			}
+
+			blockHash = computeMerkleTreeHash(hashes);
+		}
+
+		public Byte[] computeMerkleTreeHash(List<Byte[]> hashes)
+		{
+			if (hashes.Count == 1)
+				return hashes[0];
+			else if (hashes.Count == 2)
+				return PureHash.computeHash(hashes[0].Concat(hashes[1]).ToArray());
+			else
+			{
+				var mediumIndex = hashes.Count / 2;
+				List<Byte[]> firstPart = hashes.GetRange(0, mediumIndex);
+				List<Byte[]> secondPart = hashes.GetRange(mediumIndex, hashes.Count - 1);
+
+				var hashOfLeft = computeMerkleTreeHash(firstPart);
+				var hashOfRight = computeMerkleTreeHash(secondPart);
+
+				return PureHash.computeHash(hashOfLeft.Concat(hashOfRight).ToArray());
+			}
+		}
+	}
+}
--- DltNode/DltNode.Main/Program.cs
+++ DltNode/DltNode.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using DltNode.Blockchain;
 
@@ -8,13 +9,23 @@ namespace DltNode.Main
 	{
 		static void Main(string[] args)
 		{
-			Transaction tx = new Transaction("Product X has moves from A to B");
-			Console.WriteLine(tx.info);
-			//Console.WriteLine(tx.GetHash()[0]);
-			//String result = Convert.ToString(tx.GetHash());
-			Console.WriteLine(Encoding.Default.GetString(tx.GetHash()));
-			//uint a = 0;
-			//uint b = 5 / a;
+			Transaction tx1 = new Transaction("Product X has moved from A to B");
+			Transaction tx2 = new Transaction("Product Y has moved from C to D");
+
+			List<Transaction> transactions = new List<Transaction>();
+			transactions.Add(tx1);
+			transactions.Add(tx2);
+
+			Block firstBlock = new Block(transactions, null);
+			Console.WriteLine(BitConverter.ToString(firstBlock.blockHash));
+
+			Transaction tx0 = new Transaction("Product Z has moved from E to F");
+			List<Transaction> transactions1 = new List<Transaction>();
+			transactions1.Add(tx0);
+
+			Block secondBlock = new Block(transactions1, firstBlock);
+			Console.WriteLine(BitConverter.ToString(secondBlock.previousBlockHash));
+			Console.WriteLine(BitConverter.ToString(secondBlock.blockHash));
 		}
 	}
 }
