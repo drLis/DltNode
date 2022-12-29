253ea9925278f206b4deffc969fd4d8591a6fee6
dr.lis <dr.lis@outlook.com>
2022-12-29 03:35:11 +0300

    Pushed sign method in Tx class

--- DltNode/DltNode.Blockchain/Transaction.cs
+++ DltNode/DltNode.Blockchain/Transaction.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Text;
 using DltNode.Hash;
+using System.Security.Cryptography;
+
 
 namespace DltNode.Blockchain
 {
@@ -8,9 +10,32 @@ namespace DltNode.Blockchain
 	{
 		public readonly String info;
 
+		public Byte[] sinature;
+
+		public Byte[] from; // public key of sender
+
 		public Transaction(String info)
 		{
 			this.info = info;
+		}
+
+		public void Sign(RSAParameters parameters)
+		{
+			var hash = this.GetHash();
+			using (RSA rsa = RSA.Create())
+			{
+				rsa.ImportParameters(parameters);
+				RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
+				rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+
+				sinature = rsaFormatter.CreateSignature(hash);
+			}
+		}
+
+		public Boolean VerifySignature(Byte[] punlicKey)
+		{
+
+			return false;
 		}
 
 		public Byte[] GetHash() => PureHash.computeHash(Encoding.ASCII.GetBytes(info));
