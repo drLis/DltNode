b5eb996b61d401dd461731aee13ba8ba131112f4
Владислав Частухин <chastukhin.viu@students.dvfu.ru>
2023-01-04 18:35:22 +1000

    Pushed a little fix

--- DltNode/DltNode.Blockchain/Transaction.cs
+++ DltNode/DltNode.Blockchain/Transaction.cs
@@ -1,16 +1,42 @@
 ﻿using System;
 using System.Text;
 using DltNode.Hash;
+using System.Security.Cryptography;
 
 namespace DltNode.Blockchain
 {
 	public class Transaction
 	{
 		public readonly String info;
-		public Transaction(string info)
+		public Byte[] from; // public key of sender
+		public Byte[] signature;
+		public Transaction(string info,Byte[] signature = null)
 		{
 			this.info = info;
+			if (!(signature is null))
+            {
+				this.signature = signature;
+            }
 		}
+		public void Sign(RSAParameters parameters)
+        {
+			var hash = this.GetHash();
+			using (RSA rsa = RSA.Create())
+            {
+				rsa.ImportParameters(parameters);
+				///byte[] sharedParameters = rsa.ExportRSAPrivateKey();
+				RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
+				rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+
+				signature = rsaFormatter.CreateSignature(hash);
+            }
+        }
+
+		public Boolean VerifySignature(Byte[] publicKey)
+        {
+
+			return false;
+        }
 		public Byte[] GetHash() => PureHash.computeHash(Encoding.UTF8.GetBytes(info));
 	}
 }
